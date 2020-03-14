3449c81f64eee03a85e290dce8c2cdca1c9a167f
Róbert Fajd <xfajd@stuba.sk>
2020-03-14 12:24:40 +0100

    getting ready to merge

--- database_controller.py
+++ database_controller.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 import pymysql
+from datetime import date
+from datetime import datetime
 
 class Database:
+   # inicializacia prihlasovacich udajov
     def __init__(self,host,username,password,database_name):
         self.host = host
         self.password = password
         self.username = username
         self.db_name = database_name
     
+    # pripojenie do DB
     def connect(self):
         try:
             self.connection = pymysql.connect(self.host, self.username, self.password, self.db_name)
@@ -17,6 +21,7 @@ class Database:
         except:
             return 0
     
+    # dopojenie od DB
     def disconnect(self):
         try:
             self.connection.close()
@@ -24,19 +29,122 @@ class Database:
         except:
             return 0
 
-    def executeCommand(self,command):
+    # vykonanie query
+    def executeCommand(self, command):
         self.connection.ping(reconnect=True)
         self.cursor.execute(command)
         self.connection.commit()
         records = self.cursor.fetchall()
         
-       #print(records)
-        #print("[+] Done!")
+        #print(records)
+        #print("query done")
 
         if len(records) != 0:
             return records
         else:
             return 0
+
+    # registracia uzivatela
+    def addUser(self, name, secondName, password, email, loginName):
+
+        # upravenie datumu na string ddmmrrrr
+        today_date = str(date.today()).split("-")
+        reg_day = today_date[2] + today_date[1] + today_date[0]
+
+        values = "values ( '" + name + "','" + secondName + "','" + password + "','" + email + "','" + reg_day + "','" + "0" + "','" + loginName + "')"
+        command = "INSERT INTO users_list(name,second_name,password,email,reg_day,balance,login_username) " + values
+        
+        self.executeCommand(command)
+
+    # vrati vsetkych uzivatelov
+    def listUsers(self):
+        command = "SELECT name,second_name,email FROM users_list"
+        self.executeCommand(command)
+
+    # prihlasovanie pomocou mena a hesla
+    def getUserIdFromLogin(self, name, password):
+
+        command = "SELECT id FROM users_list WHERE name='" + name + "' AND password='" + password + "'"
+        out = self.executeCommand(command)
+        if type(out) == int:
+            return None
+        return out[0][0]
+
+    # ziska id z mena
+    def getUserIdFromUsername(self, name):
+
+        command = "SELECT id FROM users_list WHERE login_username='" + name + "'"
+        out = self.executeCommand(command)
+
+        # ak nenajde uzivatela vrati int nie tuple
+        if type(out) == int:
+            return None
+
+        return out[0][0]
+
+    # vrati vsetky informacie z tabulky users_list
+    def getUserInfoFromId(self, id):
+
+        command = "SELECT * FROM users_list WHERE id='" + str(id) + "'"
+        out = self.executeCommand(command)
+        return out
+    
+    # vytvori zazam o prihlaseni uzivatela
+    def createLoginLogEntry(self, user_id):
+
+        if self.getUserInfoFromId(user_id) == 0:
+            return
+
+        # upravenie casu na string hhmmss
+        now = str(datetime.now().time()).split(":")
+        now[2] = now[2].split(".")[0]
+        time = now[0] + now[1] + now[2]
+        
+        # upravenie datumu na string ddmmrrrr
+        today_date = str(date.today()).split("-")
+        date_now = today_date[2] + today_date[1] + today_date[0]
+
+        values = "('" + str(user_id) + "','" + str(date_now) + "','" + str(time) + "')"
+        command = "INSERT INTO login_log(user_id,date,time) VALUES" + values
+        self.executeCommand(command)
+    
+    # ziska akutalny stav uctu
+    def getUserBalance(self, user_id):
+
+        command = "SELECT balance FROM users_list WHERE id='" + str(user_id) + "'"
+        out = self.executeCommand(command)
+
+        return out[0][0]
+
+    # prida uzivatelovi peniaze na ucet
+    def addMoneyToUser(self, user_id, amount):
+        
+        current_balance = self.getUserBalance(user_id)
+        balance = current_balance + int(amount)
+
+        command = "UPDATE users_list SET balance=" + str(balance) + " WHERE id=" + str(user_id)
+        #print(command)
+        self.executeCommand(command)
+
+    # odoberie uzivatelovi urcenu sumu penazi
+    def removeMoneyFromUser(self, user_id, amount):
+
+        current_balance = self.getUserBalance(user_id)
+        balance = current_balance - int(amount)
+
+        command = "UPDATE users_list SET balance=" + str(balance) + " WHERE id=" + str(user_id)
+        self.executeCommand(command)
+
+    # prida zaznam o tranzakcii do logu
+    def addTransactionLog(self, sender_id, receiver_id, amount):
+
+        today_date = str(date.today()).split("-")
+        day = today_date[2] + today_date[1] + today_date[0]
+
+        values = "('" + str(sender_id) + "','" + str(receiver_id) + "'," + str(amount) + "," + str(day) + ")"
+        
+        print(command)
+        self.executeCommand(command)
 
     def executeMany(self, listToSend):
         self.connection.ping(reconnect=True)
--- main.py
+++ main.py
@@ -5,24 +5,71 @@ from config_parser import *
 
 
 class Application:
+     # pripojenie do DB, vytvorenie objektu DB
     def __init__(self):
 
         self.logged = 0
         self.user_id = None
-
+        
         db_acces = ConfigParser.getDatabaseAcces()
         self.database = Database(db_acces["host"],db_acces["user"],db_acces["password"],db_acces["database_name"])
         self.database.connect()
-        self.database.listUsers()
-        self.database.listHorses()
         print("[+] Connected to database!")
 
-    def register(self,name,secondName,password,passwordSecond,email):
+    # registracia noveho uzivatela
+    def register(self,name,secondName,password,passwordSecond,email,loginName):
+
         if password == passwordSecond:
-            self.database.addUser(name,secondName,password,email,"11223344")
+            self.database.addUser(name,secondName,password,email,loginName)
             return 1
         else:
             return 0
+
+    # prihlasenie uzivatela
+    def login(self,name,password):
+        user_id = self.database.getUserIdFromLogin(name,password)
+        if user_id != None:
+            self.database.createLoginLogEntry(user_id)
+            self.logged = 1
+            self.user_id = user_id
+            return 1
+        return 0
+
+    # vrati vsetky informacie o uzivatelovi
+    def getInfo(self):
+        out = self.database.getUserInfoFromId(self.user_id)
+        return out
+
+    # vrati stav uctu
+    def getBalance(self):
+        return self.database.getUserBalance(self.user_id)
+        
+    # nabije peniaze na ucet
+    def addMoney(self,amount):
+        self.database.addMoneyToUser(self.user_id,amount)
+
+    # odcita sumu penazi z uctu
+    def removeMoney(self, amount):
+        self.database.removeMoneyFromUser(self.user_id)
+    
+    # posle peniaze na iny ucet
+    def sendMoneyToUser(self,receiver,amount,password):
+
+        receiver_id = self.database.getUserIdFromUsername(receiver)
+        if receiver_id == None:
+            return 0
+
+        user_password = self.database.getUserInfoFromId(self.user_id)[0][3]
+        if user_password != password:
+            return 2
+
+        if self.getBalance() < amount:
+            return 3
+        
+        self.database.addMoneyToUser(receiver_id,amount)
+        self.database.removeMoneyFromUser(self.user_id,amount)
+        self.database.addTransactionLog(self.user_id,receiver_id,amount)
+        return 1
 
     def dataImport(self, id, name, sortPriority, status, venue, country, timezone):
         #print("meno", name)
